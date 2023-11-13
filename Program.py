c4b44b3e82cfd4d8cbc7f15476ef04fd791105f2
JasroopSingh <113625311+JasroopSingh@users.noreply.github.com>
2023-11-13 10:20:27 -0600

    Update Program.py

--- Program.py
+++ Program.py
@@ -14,3 +14,15 @@
     # player of the match
     # date
     # match number of the league
+
+# ----- START OF PROGRAM ----- #
+
+pip install python-cricket-scraper #using pip packet manager to install cricket scraper software
+
+from cricscraper.cricsheet import CricSheet
+sheets = Cricscraper(folder="IPL Matches Files\")
+sheets.save()
+
+
+
+
