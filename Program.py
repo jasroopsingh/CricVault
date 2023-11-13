80473c2702c520a6394af11ec14a26569019fdbd
PawitSingh <150367397+PawitSingh@users.noreply.github.com>
2023-11-13 09:40:04 -0600

    pseuodocode ben stokes

--- Program.py
+++ Program.py
@@ -1,1 +1,16 @@
-
+# figure out how to import files/data from a folder into python
+# import function on getting match id for user (can be found on ESPNcricket)
+# call for match id
+# display match info:
+    # match winner
+    # teams
+    # how team won
+    # players from both sides
+    # umpires
+    # toss winner and what they chose to do
+    # stadium/venue
+    # player scores, how they got out and to who
+    # bowling figures and extras
+    # player of the match
+    # date
+    # match number of the league
