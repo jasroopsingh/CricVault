dbee05989271aed8557c8378b288158742f3712b
JasroopSingh <113625311+JasroopSingh@users.noreply.github.com>
2023-11-21 11:43:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
 from 2023_IPL_Match_Database.py import get_match_info
 
-# Get the match information for match number 1
+# Get the match information
 match_info = get_match_info(GT_vs_CSK, 1)
 
 # Print the match information
 if match_info:
-    print(f"Match Information for Match Number {match_info['match_number']}:")
-    print(f"City: {match_info['info']['city']}")
-    print(f"Competition: {match_info['info']['competition']}")
-    print(f"Dates: {', '.join(match_info['info']['dates'])}")
-    print(f"Match Type: {match_info['info']['match_type']}")
-    print(f"Outcome: {match_info['info']['outcome']['winner']} won by {match_info['info']['outcome']['by']['wickets']} wickets")
-    print(f"Overs: {match_info['info']['overs']}")
-    print(f"Player of the Match: {', '.join(match_info['info']['player_of_match'])}")
-    print("Players:")
-    for team, players in match_info['info']['players'].items():
-        print(f"{team}:")
-        for player in players:
-            print(f"- {player}")
-else:
-    print("Match not found.")
-
-
-'''if match_info:
     print(f"Match Information for Match Number {match_info['match_number']}:")
     print(f"City: {match_info['info']['city']}")
     print(f"Competition: {match_info['info']['competition']}")
@@ -47,4 +29,4 @@ else:
         for player in players:
             print(f"- {player}")
 else:
-    print("Match not found.")'''
+    print("Match not found.")
