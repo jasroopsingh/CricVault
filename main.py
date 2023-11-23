16c45f19341f6a7b56a70b1408e778f732b9cd0f
JasroopSingh <113625311+JasroopSingh@users.noreply.github.com>
2023-11-23 16:01:06 +0000

    QoL updates

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
+print()
+print("Welcome to the IPL Archives")
+print("Maintained by Jasroop & Pawit Singh")
+print()
 print("""
 \033[1mGroup Stage\033[2m                                                                                                    
-Match 1 - GT vs CSK         Match 19 - KKR vs SRH       Match 37 - RR_vs_CSK         Match 55 - CSK_vs_DC          
-Match 2 - PBKS vs KKR       Match 20 - RCB vs DC        Match 38 - LSG_vs_PBKS       Match 56 - KKR_vs_RR           
-Match 3 - LSG vs DC         Match 21 - LSG vs PBKS      Match 39 - KKR_vs_GT         Match 57 - MI_vs_GT            
-Match 4 - RR vs SRH         Match 22 - KKR vs MI        Match 40 - DC_vs_SRH         Match 58 - SRH_vs_LSG          
-Match 5 - MI vs RCB         Match 23 - GT vs RR         Match 41 - CSK_vs_PBKS       Match 59 - DC_vs_PBKS
-Match 6 - CSK vs LSG        Match 24 - CSK vs RCB       Match 42 - RR_vs_MI          Match 60 - RCB_vs_RR
-Match 7 - DC vs GT          Match 25 - MI vs SRH        Match 43 - RCB_vs_LSG        Match 61 - CSK_vs_KKR
-Match 8 - PBKS vs RR        Match 26 - LSG vs RR        Match 44 - DC_vs_GT          Match 62 - GT_vs_SRH
-Match 9 - KKR vs RCB        Match 27 - RCB_vs_PBKS      Match 45 - LSG_vs_CSK        Match 63 - LSG_vs_MI
-Match 10 - SRH vs LSG       Match 28 - KKR_vs_DC        Match 46 - PBKS_vs_MI        Match 64 - DC_vs_PBKS
-Match 11 - RR vs DC         Match 29 - SRH_vs_CSK       Match 47 - SRH_vs_KKR        Match 65 - SRH_vs_RCB
-Match 12 - MI vs CSK        Match 30 - GT_vs_LSG        Match 48 - RR_vs_GT          Match 66 - PBKS_vs_RR
-Match 13 - GT vs KKR        Match 31 - PBKS_vs_MI       Match 49 - MI_vs_CSK         Match 67 - CSK_vs_DC
-Match 14 - PBKS vs SRH      Match 32 - RCB_vs_RR        Match 50 - RCB_vs_DC         Match 68 - LSG_vs_KKR
-Match 15 - RCB vs LSG       Match 33 - CSK_vs_KKR       Match 51 - GT_vs_LSG         Match 69 - SRH_vs_MI
-Match 16 - DC vs MI         Match 34 - DC_vs_SRH        Match 52 - RR_vs_SRH         Match 70 - RCB_vs_GT
-Match 17 - RR vs CSK        Match 35 - GT_vs_MI         Match 53 - PBKS_vs_KKR         
-Match 18 - PBKS vs GT       Match 36 - KKR_vs_RCB       Match 54 - RCB_vs_MI       
+Match 1 - GT vs CSK         Match 19 - KKR vs SRH       Match 37 - RR vs CSK         Match 55 - CSK vs DC          
+Match 2 - PBKS vs KKR       Match 20 - RCB vs DC        Match 38 - LSG vs PBKS       Match 56 - KKR vs RR           
+Match 3 - LSG vs DC         Match 21 - LSG vs PBKS      Match 39 - KKR vs GT         Match 57 - MI vs GT            
+Match 4 - RR vs SRH         Match 22 - KKR vs MI        Match 40 - DC vs SRH         Match 58 - SRH vs LSG          
+Match 5 - MI vs RCB         Match 23 - GT vs RR         Match 41 - CSK vs PBKS       Match 59 - DC vs PBKS
+Match 6 - CSK vs LSG        Match 24 - CSK vs RCB       Match 42 - RR vs MI          Match 60 - RCB vs RR
+Match 7 - DC vs GT          Match 25 - MI vs SRH        Match 43 - RCB vs LSG        Match 61 - CSK vs KKR
+Match 8 - PBKS vs RR        Match 26 - LSG vs RR        Match 44 - DC vs GT          Match 62 - GT vs SRH
+Match 9 - KKR vs RCB        Match 27 - RCB vs PBKS      Match 45 - LSG vs CSK        Match 63 - LSG vs MI
+Match 10 - SRH vs LSG       Match 28 - KKR vs DC        Match 46 - PBKS vs MI        Match 64 - DC vs PBKS
+Match 11 - RR vs DC         Match 29 - SRH vs CSK       Match 47 - SRH vs KKR        Match 65 - SRH vs RCB
+Match 12 - MI vs CSK        Match 30 - GT vs LSG        Match 48 - RR vs GT          Match 66 - PBKS vs RR
+Match 13 - GT vs KKR        Match 31 - PBKS vs MI       Match 49 - MI vs CSK         Match 67 - CSK vs DC
+Match 14 - PBKS vs SRH      Match 32 - RCB vs RR        Match 50 - RCB vs DC         Match 68 - LSG vs KKR
+Match 15 - RCB vs LSG       Match 33 - CSK vs KKR       Match 51 - GT vs LSG         Match 69 - SRH vs MI
+Match 16 - DC vs MI         Match 34 - DC vs SRH        Match 52 - RR vs SRH         Match 70 - RCB vs GT
+Match 17 - RR vs CSK        Match 35 - GT vs MI         Match 53 - PBKS vs KKR         
+Match 18 - PBKS vs GT       Match 36 - KKR vs RCB       Match 54 - RCB vs MI       
 \033[0m
 \033[1mQualifiers/Final\033[2m
 Match 71 - CSK vs GT
-Match 72 - MI_vs_LSG
-Match 73 - GT_vs_MI
+Match 72 - MI vs LSG
+Match 73 - GT vs MI
 Match 74 - CSK vs GT
 \033[0m
 """)
 
-from matchDatabase import *
-# Get the match information
-userMatch = input("Teams (Format: TEAM1vsTEAM2 or TEAM2vsTEAM1): ")
-matchNum = int(input("Match Number (Ex. 1, 2, 3, etc.): "))
+from matchDatabase import * #imports all things like funcation from matchDatabase file
+#Ask User for input - For what match do They want to display info for?
+print()
+print()
+print("\033[3m----- Please input Match for which you want to display info for -----\033[0m")
+print()
+userMatch = input("Teams (Format: TEAM1vsTEAM2 or TEAM2vsTEAM1): ").strip()
+matchNum = int(input("Match Number (Ex. 1, 2, 3, etc.): ")).strip().u()
 print()
 
 match_info = get_match_info(userMatch, matchNum)
