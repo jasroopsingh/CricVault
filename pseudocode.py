502f30b9fbdc4a2f610259bedd3d19888fba2569
JasroopSingh <113625311+JasroopSingh@users.noreply.github.com>
2023-11-23 18:26:04 -0600

    Update pseudocode.py

--- pseudocode.py
+++ pseudocode.py
@@ -15,23 +15,10 @@
     # date
     # match number of the league
 
-"""
-Retrieves the match information for a specific match number.
 
-Parameters:
-- matches: list
-    A list of match dictionaries containing the match information.
-- match_number: int
-    The match number for which the information is to be retrieved.
-
-Returns:
-- dict:
-    A dictionary containing the match information for the specified match number.
-    Returns an empty dictionary if the match number is not found.
-"""
 
 #IMPORT VARIABLES FROM ONE FILE TO ANOTHER
-    #SYNTAX: from {file name} import {function name}
+    #SYNTAX: from {file name} import {function name or *}
     #https://medium.com/geekculture/how-to-import-another-file-in-python-4f833ea462b1
 
 # ----- START OF PROGRAM ----- #
@@ -40,25 +27,3 @@ pip install python-cricket-scraper #using pip packet manager to install cricket 
 from cricscraper.cricsheet import CricSheet
 sheets = Cricscraper(folder="IPL Matches Files\")
 sheets.save()
-
-
-'''if match_info:
-    print(f"Match Information for Match Number {match_info['match_number']}:")
-    print(f"City: {match_info['info']['city']}")
-    print(f"Competition: {match_info['info']['competition']}")
-    print(f"Dates: {', '.join(match_info['info']['dates'])}")
-    print(f"Match Type: {match_info['info']['match_type']}")
-    print(f"Outcome: {match_info['info']['outcome']['winner']} won by {match_info['info']['outcome']['by']['wickets']} wickets")
-    print(f"Overs: {match_info['info']['overs']}")
-    print(f"Player of the Match: {', '.join(match_info['info']['player_of_match'])}")
-    print("Players:")
-    for team, players in match_info['info']['players'].items():
-        print(f"{team}:")
-        for player in players:
-            print(f"- {player}")
-else:
-    print("Match not found.")'''
-
-
-
-
